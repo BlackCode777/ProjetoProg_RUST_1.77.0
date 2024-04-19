d85b5a2b0653094c67cc31dc0e07324b8903ab79
Anderson Martins <BlackCode77.1980@gmail.com>
2024-04-18 22:47:30 -0300

    [ RustEstudos ] - Continuando os estudos de rust

--- EXECUCAO_de_codigo/src/main.rs
+++ EXECUCAO_de_codigo/src/main.rs
@@ -1,27 +1,4 @@
 
-use std::collections::HashMap;
-
-struct Conection {
-    driver: String,
-    str_conection: String
-}
-
 fn main(){
-
-    let mut config_db: HashMap<&str, Conection> = HashMap::new();
-
-    config_db.insert("string_postgres", Conection{
-        driver: String::from("Postgres"),
-        str_conection: "Server=IP address;Port=5432;Database=database_name;User Id=user_name;Password=password;".to_string()
-    });
-
-    config_db.insert("string_sqlserver", Conection{
-        driver: String::from("SQL Server"),
-        str_conection: "Server=IP address;Port=1433;Database=database_name;User Id=user_name;Password=password;".to_string()
-    });
-
-    for (key, value) in &config_db {
-        println!("{}: Driver: {}, Str: {}", key, value.driver, value.str_conection);
-    }
-
+    
 }
