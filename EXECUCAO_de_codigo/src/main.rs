ed8f30df9bf577d11be890d01aff6d2729ded6a3
Anderson Martins <BlackCode77.1980@gmail.com>
2024-04-26 00:33:40 -0300

    [ RustEstudos ] - Estudo de traits em rust equivalente a interfaces

--- EXECUCAO_de_codigo/src/main.rs
+++ EXECUCAO_de_codigo/src/main.rs
@@ -1,4 +1,81 @@
 
+
+// Trait
+trait Animal {
+    fn nome(&self) -> String;
+    fn som(&self) -> String;
+    fn comer(&self) -> String;
+}
+
+struct Dog;
+struct Cat;
+struct Galinha;
+
+// Struct
+struct Cachorro {
+    nome: String,
+}
+
+impl Animal for Galinha {
+    fn nome(&self) -> String {
+        "Galinha".to_string()
+    }
+
+    fn som(&self) -> String {
+        "Cocoricó".to_string()
+    }
+
+    fn comer(&self) -> String {
+        "Comendo milho".to_string()
+    }
+}
+
+impl Animal for Cat{
+    fn nome(&self) -> String {
+        "Gato".to_string()
+    }
+
+    fn som(&self) -> String {
+        "Miau".to_string()
+    }
+
+    fn comer(&self) -> String {
+        "Comendo ração".to_string()
+    }
+}
+
+// Implementação do Trait
+impl Animal for Cachorro {
+    fn nome(&self) -> String {
+        self.nome.clone()
+    }
+
+    fn som(&self) -> String {
+        "au au".to_string()
+    }
+
+    fn comer(&self) -> String {
+        "comendo ração".to_string()
+    }
+}
+
+
 fn main(){
+
+    let cachorro = Cachorro {
+        nome: "Rex".to_string(),
+    };
+
+    let gato = Cat;
+    let galinha = Galinha;
+
+    println!("{} diz {}", cachorro.nome(), cachorro.som());
+    println!("{}", cachorro.comer());
+
+    println!("{} diz {}", gato.nome(), gato.som());
+    println!("{}", gato.comer());
+
+    println!("{} diz {}", galinha.nome(), galinha.som());
+    println!("{}", galinha.comer());
     
 }
